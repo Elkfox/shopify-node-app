51922d975233939d3d0f8c9392b6df0dbc9d01f3
Popesites <conner@popesites.tech>
2019-05-20 21:37:00 -0700

    Remove duplicate app.use() calls to bodyParser.json()

--- app.js
+++ app.js
@@ -34,7 +34,6 @@ app.use(bodyParser.json({
 // uncomment after placing your favicon in /public
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
